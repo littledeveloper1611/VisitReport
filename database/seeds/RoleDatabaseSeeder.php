ba27f8966012ca87e6986d2c16e50b3fc289bb42
Nes <littledeveloper1611@gmail.com>
2018-04-02 16:42:37 +0700

    add role database seeder

---
+++ database/seeds/RoleDatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RoleDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'Salesman';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'Sales Manager';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'Director';
+        $role->save();
+    }
+}
